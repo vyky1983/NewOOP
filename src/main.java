e546527358ca49ad232995cb7ee694606f17504c
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-29 18:24:46 +0300

    add new information

--- src/main.java
+++ src/main.java
@@ -1,21 +1,13 @@
 public class main {
     public static void main(String[] args) {
-        Cat cat= new Cat();
-//        cat.age=5;
-//        cat.name="kit";
-//        cat.weight=2.03;
-        cat.getName("Tisha", 2,3.5);
-
-        Box box =new Box();
-        box.getBox(45.2,58.25,42.45);
-
-
-        Box box1 =new Box();
-        box1.getBox(58.12,98.32,78.25);
-
-
-
-
+//        Cat cat = new Cat();
+//        cat.getName("Tisha", 2, 3.5);
+//
+//        Box box = new Box();
+//        box.getBox(45.2, 58.25, 42.45);
+//
+//        Box box1 = new Box();
+//        box1.getBox(58.12, 98.32, 78.25);
 
 
     }
